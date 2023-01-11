c1abee473adbb3d012fbb06b1c892cbba2d989fd
imalhaas <lucasdecassia9@hotmail.com>
2023-01-11 19:20:47 -0300

    crud endereço

--- src/main/java/com/Attornatusapi/GerenciaDePessoas/Controller/EnderecoController.java
+++ src/main/java/com/Attornatusapi/GerenciaDePessoas/Controller/EnderecoController.java
@@ -3,10 +3,12 @@ package com.Attornatusapi.GerenciaDePessoas.Controller;
 import com.Attornatusapi.GerenciaDePessoas.Model.Endereco;
 import com.Attornatusapi.GerenciaDePessoas.Repository.EnderecoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -21,5 +23,11 @@ public class EnderecoController {
         return enderecoRepository.findAll();
     }
 
-    
+    @PostMapping
+    public ResponseEntity<Endereco> criar(@Valid @RequestBody Endereco endereco, HttpServletResponse response){
+        Endereco enderecoSalvo = enderecoRepository.save(endereco);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(enderecoSalvo);
+    }
+
 }
